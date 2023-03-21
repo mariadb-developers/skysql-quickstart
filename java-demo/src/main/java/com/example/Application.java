bbac60d24e5be2b970fa49d90785d25150594dd0
Alejandro Duarte <alejandro.d.a@gmail.com>
2023-03-21 20:50:25 +0200

    Use var in Java demo

--- java-demo/src/main/java/com/example/Application.java
+++ java-demo/src/main/java/com/example/Application.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,7 +8,7 @@ import java.sql.SQLException;
 public class Application {
 	
 	public static void main(String[] args) throws SQLException {
-		try (Connection connection = DriverManager.getConnection(
+		try (var connection = DriverManager.getConnection(
 			"jdbc:mariadb://dbpgf24938486.sysp0000.db.skysql.net:3306/demo?sslMode=1&serverSslCert=/Users/alejandro/Downloads/skysql_chain_2022.pem", "dbpgf24938486", "1%E5pTCkW7.SFCZD27P4x7")) {
 			PreparedStatement statement = connection.prepareStatement("select * from messages");
 			ResultSet rs = statement.executeQuery();
