2e0e83e4a558d7acd0479f84dde32b2213a50f9a
Alejandro Duarte <alejandro.d.a@gmail.com>
2023-03-21 14:51:57 +0200

    Refactor

--- java-demo/src/main/java/com/example/Application.java
+++ java-demo/src/main/java/com/example/Application.java
@@ -1,21 +1,21 @@
 package com.example;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Application {
-
-	public static void main(String... args) throws SQLException {
-		System.out.println("Messages in the database:\n");
-		try (var connection = DriverManager.getConnection(
-				"jdbc:mariadb://xxxxxx.yyyyy.db.skysql.net:3306/demo?sslMode=1&serverSslCert=/Users/alejandro/Downloads/skysql_chain_2022.pem",
-				"user", "password")) {
-
-			var statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from messages");
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString("content"));
+	
+	public static void main(String[] args) throws SQLException {
+		try (Connection connection = DriverManager.getConnection(
+			"jdbc:mariadb://dbpgf24938486.sysp0000.db.skysql.net:3306/demo?sslMode=1&serverSslCert=/Users/alejandro/Downloads/skysql_chain_2022.pem", "dbpgf24938486", "1%E5pTCkW7.SFCZD27P4x7")) {
+			PreparedStatement statement = connection.prepareStatement("select * from messages");
+			ResultSet rs = statement.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getString("content"));
 			}
 		}
 	}
