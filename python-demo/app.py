690434ad82cde5617982cd5b97d5f9c481e252e0
Alejandro Duarte <alejandro.d.a@gmail.com>
2023-03-21 13:44:21 +0200

    Exception handling in Python demo

--- python-demo/app.py
+++ python-demo/app.py
@@ -1,16 +1,19 @@
 import mariadb
 
-connection = mariadb.connect(
-    host="dbpgf24938486.sysp0000.db.skysql.net",
-    port=3306,
-    database="demo",
-    user="dbpgf24938486",
-    password="1%E5pTCkW7.SFCZD27P4x7",
-    ssl_ca="/Users/alejandro/Downloads/skysql_chain_2022.pem"
-)
+try:
+	with mariadb.connect(
+		host="dbpgf24938486.sysp0000.db.skysql.net",
+		port=3306,
+		database="demo",
+		user="dbpgf24938486",
+		password="1%E5pTCkW7.SFCZD27P4x7",
+		ssl_ca="/Users/alejandro/Downloads/skysql_chain_2022.pem"
+	) as connection:
+		cursor = connection.cursor()
+		cursor.execute("select * from messages")
 
-cursor = connection.cursor()
-cursor.execute("select * from messages")
+		for (id, content) in cursor:
+			print(content)
 
-for (id, content) in cursor:
-	print(content)
+except Exception as ex:
+	print(ex)
